b0955195eb65628d0f81f390307bfcb99520d0a2
Magamed2009 <muhammadmkr6@gmail.com>
2023-12-15 16:12:05 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿// Task 1
+// int a;
+// void sum(int a)
+// {
+//     for(int i=a; i!=0; i--) {System.Console.Write($"{i} ");}
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// sum(a);
+
+// Task 2
+// int a,b,c,d;
+// int sum(int a,int b,int c,int d)
+// {
+//     return a+b+c+d;
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// b=Convert.ToInt32(Console.ReadLine());
+// c=Convert.ToInt32(Console.ReadLine());
+// d=Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(sum(a,b,c,d));
+
+// Task 3 (1)
+// int a;
+// void sum(int a)
+// {
+//     for(int i=0; i<a; i++) {System.Console.Write("*");}
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// sum(a);
+
+// Task 3 (2)
+// int a;
+// void sum(int a)
+// {
+//     for(int i=0; i<a; i++) 
+//     {
+//         System.Console.WriteLine();
+//         for(int j=0; j<a-1; j++) {System.Console.Write("*");}
+//         System.Console.Write("*");
+//     }
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// sum(a);
+
+// Task 3 (3)
+// int a,b;
+// void sum(int a, int b)
+// {
+//     for(int i=0; i<b; i++) 
+//     {
+//         System.Console.WriteLine();
+//         for(int j=0; j<a-1; j++) {System.Console.Write("*");}
+//         System.Console.Write("*");
+//     }
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// b=Convert.ToInt32(Console.ReadLine());
+// sum(a,b);
+
+// Task 4 
+// int a;
+// void sum(int a)
+// {
+//     for(int i=1; i<=a; i++)
+//     {
+//         for(int j=1; j<=i; j++) {System.Console.Write("*");}
+//         System.Console.WriteLine();
+//     }
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// sum(a);
+
+// Task 4 (1)
+// int a;
+// void sum(int a)
+// {
+//     for(int i=1; i<=a; i++)
+//     {
+//         for(int j=1; j<=a-i; j++) {System.Console.Write(" ");}
+//         for(int k=0; k<i; k++) {System.Console.Write("*");}
+//         System.Console.WriteLine();
+//     }
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// sum(a);
+
+// Task (2)
+
+
+// Task 5
+// int a;
+// void sum(int a)
+// {
+//     while(a!=0)
+//     {
+//         System.Console.Write($"{a%10}");
+//         a/=10;
+//     }
+// }
+// a=Convert.ToInt32(Console.ReadLine());
+// sum(a);
